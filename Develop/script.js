876a84a738006dbf8e28078e5b8bf89561e5abdc
patel.akash30@icloud.com <patel.akash30@icloud.com>
2022-10-31 15:09:16 -0400

    changes to html

--- Develop/script.js
+++ Develop/script.js
@@ -12,20 +12,71 @@ var symArr = sym.split("");
  
 
 // initial instruction for the user
-window.onload = alert("Welcome! To begin please click 'Generate Password");
+window.onload = alert("Welcome! To begin please click ok");
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+function generatepass(){
+  var allCHars = [];
+  var resultPass = "";
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+var Totlength = prompt("How many characters would you like your password to be?");
+
+if(Totlength <8 || Totlength >128){
+  alert("We recommend to have a password between 8 and 128 characters \nplease start over.");
+}
+
+else{
+  if(confirm("would you like your password to contain upper case letters?")){
+    Array.prototype.push.apply(AllChars, abcUpperArr);
+  }
+
+  if(confirm("would you like your passoword to contain lower case letters?")){
+    Array.prototype.push.apply(AllChars, abcUpperArr);
+  }
+
+  if(confirm("would you like your password to contain numbers?")){
+    Array.prototype.push.apply(allChars, abcUpperArr);
+  }
+
+  if(confirm("Would you like your password to contain symbols?")){
+    Array.prototype.push.apply(allChars, symArr);
+  }
+
+  if(allChars.length===0){
+    alert("You must select at lease 1 type of characters to generate a password!\nPlease start over.");
+  }
+
+  
+  else{
+    for(var i=0; i<Totlength; i++){
+        var random = Math.floor(Math.random()*allChars.length);
+        resultPass += allChars[random];
+    }
+}
+}
+  
+
+        document.getElementById("password").innerHTML = resultPass;
+
+        function writePassword() {
+          var password = generatePassword();
+          var passwordText = document.querySelector("#password");
+        
+          passwordText.value = password;
+        
+        }
+        
+        // Add event listener to generate button
+        generateBtn.addEventListener("click", writePassword);
+  
+
+function copyPass(){
+
+  document.querySelector("textarea").select();
+  document.execCommand("Copy");
+  alert("Password copied to clipboard!");
+}
